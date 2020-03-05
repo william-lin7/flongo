6349ebdc3f5f6a8bf5957aab2395b13a4ca00d80
William Lin <wlin01@stuy.edu>
2020-03-05 09:16:51 -0500

    updated

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from pymongo import MongoClient
+from pprint import pprint
+from bson.json_util import loads
+import json
+import datetime
+app = Flask(__name__)
+
+client = MongoClient()
+db = client.WhoLetTheDogsOut
+meteorites = db.meteorites
+meteorites.delete_many({})
+
+with open("meteorites.json", 'r') as file:
+    data = json.load(file)
+    for member in data:
+        id = meteorites.insert_one(loads(json.dumps(member)))
+        print(id)
+
+@app.route("/")
+def home():
+    return findName("Aachen")
+
+
+def findName(name):
+        for meteorite in meteorites.find({"name" : str(name)}):
+                pprint(meteorite)
+
+
+def findMass(mass):
+        for meteorite in meteorites.find({"mass": str(mass)}):
+                pprint(meteorite)
+
+
+def findYear(year):
+        i = 0
+        for meteorite in meteorites.find():
+                try:
+                        yeartemp = meteorite["year"][:4]
+                        if (yeartemp == str(year)):
+                                pprint(meteorite)
+                except: pass
+
+
+def findCoordinates(lat, long):
+        for meteorite in meteorites.find({"$and":[
+                        {"reclat" : { "$gt": str(lat-2), "$lt": str(lat+2)}},
+                        {"reclong" : { "$gt": str(long-2), "$lt": str(long+2)}}
+                        ]}):
+                pprint(meteorite)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
